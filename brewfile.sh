bf14985204fa47c353a5e214413c156fce89db4c
naoki_ichihara <naoki_ichihara@sobal.co.jp>
2016-03-16 23:16:15 +0900

    Add android-file-transfer

--- brewfile.sh
+++ brewfile.sh
@@ -238,6 +238,7 @@ brew cask install --appdir=/Applications jxplorer || true
 brew cask install --appdir=/Applications sdformatter || true
 brew cask install --appdir=/Applications clipmenu || true
 brew cask install --appdir=/Applications 0xdbe-eap || true
+brew cask install android-file-transfer || true
 
 # caskroom/versions
 brew cask install --appdir=/Applications macvim-kaoriya || true
